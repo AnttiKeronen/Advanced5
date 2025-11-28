48b6a6abbf4ddf99865e8885066e705de09f9cbd
AnttiKeronen <antti.keronen@student.lut.fi>
2025-11-28 15:51:40 +0200

    jeeesjees

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,28 +1,18 @@
 import mongoose, { Schema, Document } from "mongoose";
-
-/* ----------------------- ITodo INTERFACE ----------------------- */
 export interface ITodo {
   todo: string;
   checked: boolean;
 }
-
-/* ----------------------- IUser INTERFACE ----------------------- */
 export interface IUser extends Document {
   name: string;
   todos: ITodo[];
 }
-
-/* -------------------------- TODO SCHEMA ------------------------ */
 const TodoSchema = new Schema<ITodo>({
   todo: { type: String, required: true },
   checked: { type: Boolean, default: false }
 });
-
-/* -------------------------- USER SCHEMA ------------------------ */
 const UserSchema = new Schema<IUser>({
   name: { type: String, required: true },
   todos: { type: [TodoSchema], default: [] }
 });
-
-/* -------------------------- MODEL EXPORT ----------------------- */
 export const User = mongoose.model<IUser>("User", UserSchema);
